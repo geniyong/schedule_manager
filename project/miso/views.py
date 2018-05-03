1ce047faa3056f5b1964c1192764d77ce9532cf2
Jaeyong Cheon <35215439+geniyong@users.noreply.github.com>
2018-05-03 13:53:28 +0900

    Update views.py

--- project/miso/views.py
+++ project/miso/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+class PossibleList(TemplateView):
+	template_name = ‘main.html’
+
+	def get_context_data(self,**wargs):
+		qs=
