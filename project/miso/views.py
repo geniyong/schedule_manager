bf2d0a8222ecc1e6de1af2ebc8da4bdfaeb1cc9e
Jaeyong Cheon <35215439+geniyong@users.noreply.github.com>
2018-05-03 14:59:13 +0900

    Update views.py

--- project/miso/views.py
+++ project/miso/views.py
@@ -6,5 +6,10 @@ from .models import *
 class PossibleList(TemplateView):
 	template_name = ‘main.html’
 
-	def get_context_data(self,**wargs):
+	def get_context_data(self,**kwargs):
 		qs=Possible_schedule.objects.all
+		
+		context = super(PossibleList, self).get_context_data(**kwargs)
+		context[‘PossilbeAll’] = qs
+		
+		return context
